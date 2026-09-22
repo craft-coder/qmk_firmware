efa15337e6a6dc7f235735355f1edaeefff86688
Dr. Marius Feilhauer <software@feilhauer.de>
2026-09-22 20:04:06 +0200

    Added COMBO_ONLY_FROM_LAYER

--- keyboards/silakka54/keymaps/neo/keymap.c
+++ keyboards/silakka54/keymaps/neo/keymap.c
@@ -31,6 +31,8 @@
 #define SINGLQ LSFT(KC_NUHS)
 #define SEMICO LSFT(KC_COMM)
 
+#define COMBO_ONLY_FROM_LAYER 0
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
